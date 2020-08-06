a6b117818926c72d3b5f2d4f863643d79743734a
dsoto-uy <clever.soto@gmail.com>
2020-08-06 02:51:11 -0300

    Entrega 1 - commit 1
    Listado de productos hecho, login hecho, google auth sin terminar.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,44 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+const CLIENT_ID = "880069710415-vp1tu8ijf94aoc1u5pu68gbtlfa6jn27.apps.googleusercontent.com";
 
+document.addEventListener("DOMContentLoaded", function(e){
+    showGoogleButton();
+    startApp();
 });
+
+function showGoogleButton(){
+
+    let htmlContentToAppend = "";
+    
+   /* htmlContentToAppend += `<button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Iniciar Sesion con Google</button>`*/
+htmlContentToAppend += `<button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2 customGPlusSignIn"></i> Iniciar Sesion con Google</button>`
+
+    document.getElementById("botonGoogle").innerHTML = htmlContentToAppend;
+}
+
+var googleUser = {};
+  var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: CLIENT_ID,
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('botonGoogle'));
+    });
+  };
+
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+          document.getElementById('botonGoogle').innerText = "Signed in: " +
+              googleUser.getBasicProfile().getName();
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
--- js/products.js
+++ js/products.js
@@ -1,6 +1,49 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var productsArray = [];
 
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;            
+            showProductsList(productsArray);                        
+        }      
+    });        
 });
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">                    
+                </div>
+                <div class="col hasProductCostst">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>                                                                                              
+                        <small class="text-muted">` + product.soldCount + ` vendidos</small>
+                    </div>
+                    <div>
+                        <p>` + product.description +`</p>                                               
+                    </div>
+                    <div class= "bottomCost">
+                        <h3>` + product.cost + ` ` + product.currency + `</h3>
+                    </div>
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
+}
