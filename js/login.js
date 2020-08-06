b3213c86b1bf3f17b99705ac01b97ea0850fe945
dsoto-uy <clever.soto@gmail.com>
2020-08-06 04:58:44 -0300

    Entrega 1 - commit 3 - redireccion auth
    Prueba de redirección

--- js/login.js
+++ js/login.js
@@ -4,8 +4,7 @@
 const CLIENT_ID = "880069710415-2f4rpegucor1qqghmgke263m6h8g3k39.apps.googleusercontent.com";
 
 document.addEventListener("DOMContentLoaded", function(e){
-    showGoogleButton();
-    startApp();
+    showGoogleButton();   
 });
 
 function showGoogleButton(){
@@ -13,27 +12,24 @@ function showGoogleButton(){
     let htmlContentToAppend = "";
     
    /* htmlContentToAppend += `<button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Iniciar Sesion con Google</button>`*/
-htmlContentToAppend += `<button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2 customGPlusSignIn"></i> Iniciar Sesion con Google</button>`
+htmlContentToAppend += `<button class="btn btn-lg btn-google btn-block text-uppercase" type="submit" onClick="signIn()"><i class="fab fa-google mr-2 customGPlusSignIn""></i> Iniciar Sesion con Google</button>`
 
     document.getElementById("botonGoogle").innerHTML = htmlContentToAppend;
 }
 
 var googleUser = {};
-  var startApp = function() {
-    gapi.load('auth2', function(){
-      // Retrieve the singleton for the GoogleAuth library and set up the client.
+  function signIn() {    
+    gapi.load('auth2', function(){            
       auth2 = gapi.auth2.init({
-        client_id: CLIENT_ID,
-        redirect_uri: 'https://dsoto-uy.github.io/repo-jap/index.html',
-        cookiepolicy: 'single_host_origin',
-        // Request scopes in addition to 'profile' and 'email'
-        //scope: 'additional_scope'
+        client_id: CLIENT_ID,                
+        cookiepolicy: 'single_host_origin',      
       });
-      attachSignin(document.getElementById('botonGoogle'));
+      alert("asdasds");
+      alert(googleUser.getBasicProfile().getName());
     });
   };
 
-  function attachSignin(element) {
+  /*function attachSignin(element) {
     console.log(element.id);
     auth2.attachClickHandler(element, {},
         function(googleUser) {
@@ -42,4 +38,4 @@ var googleUser = {};
         }, function(error) {
           alert(JSON.stringify(error, undefined, 2));
         });
-  }
+  }*/
